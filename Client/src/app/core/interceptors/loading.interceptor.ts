e9d7a00d006b317e3058fe5655ffbbc530188314
Rahulkumar <rahulkumar.yadav@audreeinfotech.com>
2024-01-06 16:18:12 +0530

    End of Section 12

---
+++ Client/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, delay, finalize, timeout } from 'rxjs';
+import { BusyService } from '../services/busy.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private spinnerService: BusyService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    this.spinnerService.busy();
+    return next.handle(request).pipe(
+      delay(2000),
+      finalize(() => {
+        this.spinnerService.idle();
+      })
+    );
+  }
+}
--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,8 +13,11 @@ export class ProductDetailsComponent implements OnInit {
   product?: Product;
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -22,7 +26,10 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     let productId: number = +this.activatedRoute.snapshot.paramMap.get('id')!;
     this.shopService.getProduct(productId).subscribe({
-      next: (product) => (this.product = product),
+      next: (product) => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name);
+      },
       error: (error) => console.log(error),
     });
   }
